f16c9996339f01e54dc0bae9371004c780ec754d
pokerc <pokerc@126.com>
2017-12-06 15:20:22 +0800

    Wed Dec  6 15:20:11 CST 2017

--- tensorflow/cnn_sample_tensorflow2017.py
+++ tensorflow/cnn_sample_tensorflow2017.py
@@ -17,19 +17,21 @@ np.random.seed(1)
 BATCH_SIZE = 50
 LR = 0.001              # learning rate
 
-mnist = input_data.read_data_sets('./mnist', one_hot=True)  # they has been normalized to range (0,1)
+mnist = input_data.read_data_sets('./MNIST_data', one_hot=True)  # they has been normalized to range (0,1)
 test_x = mnist.test.images[:2000]
 test_y = mnist.test.labels[:2000]
+# print(mnist,mnist.test.images.shape,test_y[0])
 
-# plot one example
-print(mnist.train.images.shape)     # (55000, 28 * 28)
-print(mnist.train.labels.shape)   # (55000, 10)
-plt.imshow(mnist.train.images[0].reshape((28, 28)), cmap='gray')
-plt.title('%i' % np.argmax(mnist.train.labels[0])); plt.show()
+# # plot one example
+# print(mnist.train.images.shape)     # (55000, 28 * 28)
+# print(mnist.train.labels.shape)   # (55000, 10)
+# plt.imshow(mnist.train.images[0].reshape((28, 28)), cmap='gray')
+# plt.title('%i' % np.argmax(mnist.train.labels[0])); plt.show()
 
 tf_x = tf.placeholder(tf.float32, [None, 28*28]) / 255.
 image = tf.reshape(tf_x, [-1, 28, 28, 1])              # (batch, height, width, channel)
 tf_y = tf.placeholder(tf.int32, [None, 10])            # input y
+
 
 # CNN
 conv1 = tf.layers.conv2d(   # shape (28, 28, 1)
---
+++ tensorflow/javen_cnn_sample.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+tf.set_random_seed(1)
+np.random.seed(1)
+
+BATCH_SIZE = 50
+LR = 0.001
+
